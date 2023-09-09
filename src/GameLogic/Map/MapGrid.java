1360c6723466802355673b582adbfd009e0d91d9
Kevin Huang <90675779+inner-LMNt@users.noreply.github.com>
2023-09-09 04:18:01 +0000

    Update MapGrid.java

--- src/GameLogic/Map/MapGrid.java
+++ src/GameLogic/Map/MapGrid.java
@@ -6,15 +6,37 @@ import lombok.Data;
 @Data
 @Builder
 public class MapGrid {
-    int[][] grid;
-    public void setupGrid(int size){
-        grid = new int[size][size];
-        for(int i = 0; i<size; i++){
-            for(int j = 0; j<size; j++){
+    private int[][] grid;
+    private int gridSize;
+
+    public MapGrid(int size) {
+        gridSize = size;
+        setupGrid();
+    }
+    
+    public void setupGrid() {
+        grid = new int[gridSize][gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
                 grid[i][j] = 0;
             }
         }
+    }
 
+    public void setCellValue(int x, int y, int value) {
+        if (isValidCell(x, y)) {
+            grid[x][y] = value;
+        }
+    }
 
+    public int getCellValue(int x, int y) {
+        if (isValidCell(x, y)) {
+            return grid[x][y];
+        }
+        return -1;
+    }
+
+    public boolean isValidCell(int x, int y) {
+        return x >= 0 && x < gridSize && y >= 0 && y < gridSize;
     }
 }
